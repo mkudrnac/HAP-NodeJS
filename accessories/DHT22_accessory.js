436ee3ae465df0d3c22c921ce6fcb4d6d70402d3
mkudrnac <miroslav.kudrnac@rohlik.cz>
2017-11-27 21:53:55 +0100

    + DHT22

--- accessories/DHT22_accessory.js
+++ accessories/DHT22_accessory.js
@@ -16,12 +16,12 @@ var DHT22_SENSOR = {
   },
   getTemperature: function() {
     var temp = DHT22_SENSOR.currentTemperature;
-    console.log("Getting the current temperature: ", temp);
+    console.log("Getting the current temperature:", temp);
     return temp;
   },
   getHumidity: function () {
     var humidity = DHT22_SENSOR.currentHumidity;
-    console.log("Getting the current humidity: ", humidity);
+    console.log("Getting the current humidity:", humidity);
     return humidity;
   }
 };
